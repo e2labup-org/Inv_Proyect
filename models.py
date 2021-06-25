275c2588576647394ee2f6a5f6c34e46f53f1819
Sergio2405 <67673799+Sergio2405@users.noreply.github.com>
2021-06-25 17:43:06 -0500

    Update models.py

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ from otree.api import (
     Currency as c,
     currency_range,
 )
-
+import random
 
 doc = """
 This is a standard 2-player trust game where the amount sent by player 1 gets
@@ -32,14 +32,13 @@ class Constants(BaseConstants):
 
 class Subsession(BaseSubsession):
     def creating_session(self):
-        import random
 
         num = random.random()
 
         defectuoso = 1 if num >0.5 else 0
-        asset = c(5) if defectuoso == 0 else c(15)
+        asset = c(5) if random.random() > 0.5 else c(15)
 
-        self.session.vars['asset'] = asset
+        self.group.asset_value = asset
 
 class Group(BaseGroup):
     monto_enviado = models.CurrencyField(
@@ -54,6 +53,8 @@ class Group(BaseGroup):
         max=Constants.endowment_comprador,
         label="Monto a PAGAR por el activo (indicar un monto si deseas adquirirlo, 0 de otro modo)"
     )
+    
+    asset_value = models.CurrencyField()
 
     def sent_back_amount_max(self):
         return self.sent_amount 
@@ -61,8 +62,8 @@ class Group(BaseGroup):
     def set_payoffs(self):
         p1 = self.get_player_by_id(1)
         p2 = self.get_player_by_id(2)
-        p1.payoff = Constants.endowment_vendedor - self.session.vars['asset'] + self.monto_enviado #Utilidad del Vendedor
-        p2.payoff =  Constants.endowment_comprador + self.session.vars['asset'] - self. monto_pago #Utilidad del comprador
+        p1.payoff = Constants.endowment_vendedor - self.asset_value + self.monto_enviado #Utilidad del Vendedor
+        p2.payoff =  Constants.endowment_comprador + self.asset_value - self. monto_pago #Utilidad del comprador
 
 
 class Player(BasePlayer):
